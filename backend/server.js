a9270c9c6b621fec6ea87f122f8796f5f9e2cf5d
Sayli Santosh Kulkarni <159431703+saylik365@users.noreply.github.com>
2025-08-06 13:06:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://linkup-1-08s6.onrender.com' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
