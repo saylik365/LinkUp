e5517162c645a15ace26ff38a921a3654d7aef4e
Sayli Santosh Kulkarni <159431703+saylik365@users.noreply.github.com>
2025-08-06 12:48:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,16 @@ const allowedOrigins = [
 ];
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
